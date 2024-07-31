c32f0d0f344db9466753a552833e387e9f38c08f
katyastek <katyas@campus.technion.ac.il>
2024-07-31 23:30:36 +0000

    with part 2

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -3,5 +3,95 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-  ####### Some smart student's code here #######
+  # we dont really know how 'what_to_do'works, 
+  # so we're saving all the regs
+  pushq %rbp
+  pushq %rsp
+  pushq %rsi
+  pushq %rdi
+  pushq %rax
+  pushq %rbx
+  pushq %rcx
+  pushq %rdx
+  pushq %r8
+  pushq %r9
+  pushq %r10
+  pushq %r11
+  pushq %r12
+  pushq %r13
+  pushq %r14
+  pushq %r15
+
+  movq $0, %rax
+  movq $0, %rbx
+  movq $0, %rdi
+
+  movq 128(%rsp), %rax  # 8 bytes*16 regs = 128
+  movb (%rax), %al      # loads first byte
+
+  cmp $0x0f, %al
+  jne byte_command
+
+# else it's a 2 byte command, these are the only 2 options
+two_byte_command:
+  movb 1(%rax), %al     # loads last byte
+  movq $2, %rbx         # contains the len of the opcode
+  jmp call_handling
+
+byte_command:
+  movq $1, %rbx         # contains the len of the opcode
+  jmp call_handling
+
+call_handling: 
+  movq %rax, %rdi
+  pushq %rbx
+
+  call what_to_do
+  
+  popq %rbx
+  test %rax, %rax
+  jz original
+  jmp special
+
+original:
+  popq %r15             # poping from end to start
+  popq %r14
+  popq %r13
+  popq %r12
+  popq %r11
+  popq %r10
+  popq %r9
+  popq %r8
+  popq %rdx
+  popq %rcx
+  popq %rbx
+  popq %rax
+  popq %rdi
+  popq %rsi
+  popq %rsp
+  popq %rbp
+  
+  jmp (old_ili_handler)  # need to check if works
+
+special:
+  movq %rax, %rdi        # specifically asked in the pdf
+  addq $rbx, 128(%rsp)   # next command
+  
+  popq %r15
+  popq %r14
+  popq %r13
+  popq %r12
+  popq %r11
+  popq %r10
+  popq %r9
+  popq %r8
+  popq %rdx
+  popq %rcx
+  popq %rbx
+  popq %rax
+  popq %rdi
+  popq %rsi
+  popq %rsp
+  popq %rbp
+  
   iretq
--- part2/ili_utils.c
+++ part2/ili_utils.c
@@ -1,25 +1,39 @@
 #include <asm/desc.h>
 
-void my_store_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// </STUDENT FILL>
+void my_store_idt(struct desc_ptr *idtr)
+{
+    unsigned long idt[2];
+    asm volatile("sidt %0":"=m"(idt));
+    idtr->size = idt[0];
+    idtr->address = idt[1];
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// <STUDENT FILL>
+    asm volatile("lidt %0"::"m"(*idtr):"memory");
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
+    unsigned long high = addr >> 32;
+    unsigned long middle = addr >> 16;
+    unsigned long low = addr;
 
-// </STUDENT FILL>
+    gate->offset_high = high;
+    gate->offset_middle = middle;
+    gate->offset_low = low;
+    
 }
 
+# the opposite of the previous function
 unsigned long my_get_gate_offset(gate_desc *gate) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
+    unsigned long high = gate->offset_high;
+    unsigned long middle = gate->offset_middle;
+    unsigned long low = gate->offset_low;
+    unsigned long addr = 0;
+    
+    addr += high << 32;
+    addr += middle << 16;
+    addr += low;
 
-// </STUDENT FILL>
+    return addr;
+
 }
