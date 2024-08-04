14ec87162fcebe021e9521fdc40bacbd6daeb253
katyastek <katyas@campus.technion.ac.il>
2024-08-04 15:12:39 +0000

    part 2 updated

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -5,8 +5,6 @@
 my_ili_handler:
   # we dont really know how 'what_to_do'works, 
   # so we're saving all the regs
-  pushq %rbp
-  pushq %rsp
   pushq %rsi
   pushq %rdi
   pushq %rax
@@ -21,62 +19,45 @@ my_ili_handler:
   pushq %r13
   pushq %r14
   pushq %r15
+  pushq %rbp
+  pushq %rsp
 
   movq $0, %rax
   movq $0, %rbx
+  movq $0, %rcx
   movq $0, %rdi
 
-  movq 128(%rsp), %rax  # 8 bytes*16 regs = 128
-  movb (%rax), %al      # loads first byte
+  movq 128(%rsp), %rbx  # 8 bytes*16 regs = 128
+  movb (%rbx), %al      # move first byte to al   
 
   cmp $0x0f, %al
   jne byte_command
 
 # else it's a 2 byte command, these are the only 2 options
 two_byte_command:
-  movb 1(%rax), %al     # loads last byte
-  movq $2, %rbx         # contains the len of the opcode
+  movb 1(%rbx), %al         # loads second(last) byte
+  movq $2, %rcx         # contains the len of the opcode
   jmp call_handling
 
 byte_command:
-  movq $1, %rbx         # contains the len of the opcode
-  jmp call_handling
+  movq $1, %rcx         # contains the len of the opcode
 
 call_handling: 
   movq %rax, %rdi
-  pushq %rbx
+  pushq %rcx
 
   call what_to_do
   
-  popq %rbx
+  popq %rcx
   test %rax, %rax
   jz original
-  jmp special
-
-original:
-  popq %r15             # poping from end to start
-  popq %r14
-  popq %r13
-  popq %r12
-  popq %r11
-  popq %r10
-  popq %r9
-  popq %r8
-  popq %rdx
-  popq %rcx
-  popq %rbx
-  popq %rax
-  popq %rdi
-  popq %rsi
-  popq %rsp
-  popq %rbp
-  
-  jmp (old_ili_handler)  # need to check if works
 
 special:
   movq %rax, %rdi        # specifically asked in the pdf
-  addq %rbx, 128(%rsp)   # next command
   
+  popq %rsp
+  popq %rbp
+  addq %rcx, 112(%rsp)   # next command
   popq %r15
   popq %r14
   popq %r13
@@ -89,9 +70,27 @@ special:
   popq %rcx
   popq %rbx
   popq %rax
-  popq %rdi
   popq %rsi
-  popq %rsp
-  popq %rbp
+  popq %rsi
   
   iretq
+
+original:
+  popq %rsp
+  popq %rbp             # poping from end to start
+  popq %r15             
+  popq %r14
+  popq %r13
+  popq %r12
+  popq %r11
+  popq %r10
+  popq %r9
+  popq %r8
+  popq %rdx
+  popq %rcx
+  popq %rbx
+  popq %rax
+  popq %rdi
+  popq %rsi
+  
+  jmp * old_ili_handler  # need to check if works
--- part2/ili_utils.c
+++ part2/ili_utils.c
@@ -1,11 +1,7 @@
 #include <asm/desc.h>
 
-void my_store_idt(struct desc_ptr *idtr)
-{
-    unsigned long idt[2];
-    asm volatile("sidt %0":"=m"(idt));
-    idtr->size = idt[0];
-    idtr->address = idt[1];
+void my_store_idt(struct desc_ptr *idtr){
+    asm volatile("sidt %0":"=m"(*idtr)::);
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
@@ -20,7 +16,6 @@ void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
     gate->offset_high = high;
     gate->offset_middle = middle;
     gate->offset_low = low;
-    
 }
 
 //the opposite of the previous function
@@ -29,11 +24,8 @@ unsigned long my_get_gate_offset(gate_desc *gate) {
     unsigned long middle = gate->offset_middle;
     unsigned long low = gate->offset_low;
     unsigned long addr = 0;
-    
-    addr += high << 32;
-    addr += middle << 16;
-    addr += low;
+
+    addr = (high << 32) + (middle << 16) + low;
 
     return addr;
-
 }
